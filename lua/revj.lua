a858d40953d91fe91dd9907a8bbf34993acdba86
Axel Dahlberg <git@valleymnt.com>
2021-03-27 20:07:41 +0100

    Fixing some issues for certain patterns

--- lua/revj.lua
+++ lua/revj.lua
@@ -3,6 +3,7 @@ local revj = {}
 local settings = {
     brackets = {first = '([{<', last = ')]}>'},
     new_line_before_last_bracket = true,
+    add_seperator_for_last_parameter = true,
     enable_default_keymaps = false,
     keymaps = {},
 }
@@ -35,19 +36,46 @@ local normal = function(keys, opts)
     vim.cmd(replace_termcodes(cmd.." "..keys))
 end
 
+local select_and_return_inner_parameter = function()
+    normal('yi'..vim.g.vim_textobj_parameter_mapping, {noremap = false})
+    return vim.fn.getreg('""')
+end
+
+local select_and_return_outer_parameter = function()
+    normal('ya'..vim.g.vim_textobj_parameter_mapping, {noremap = false})
+    return vim.fn.getreg('""')
+end
+
 local getcurchar = function()
     normal('vy')
     return vim.fn.getreg('""')
 end
 
+local is_end_bracket = function(char)
+    return vim.fn.match(settings.brackets.last, char) >= 0
+end
+
+local is_last_parameter_of_expression = function()
+    select_and_return_outer_parameter()
+    normal('`]l') -- go character after parameter
+    local cur_char = getcurchar()
+    return is_end_bracket(cur_char)
+end
+
+local is_cur_parameter_empty = function()
+    local parameter = select_and_return_outer_parameter()
+    normal('`]')
+    return vim.fn.match(parameter, '^\\s*'..SEPERATOR..'\\s*$') >= 0
+end
+
 local add_last_colon_if_missing = function(end_pos)
     -- check if either current or next character is , otherwise insert it
     vim.fn.setpos('.', end_pos)
-    normal('ya,`]', {noremap = false}) -- go to end of arg and get last char
-    -- since vim-textobj-parameter considers , followed by blankspace an argument
+    local parameter = select_and_return_outer_parameter()
+    normal('`]') -- go to end of parameter and get last char
+    -- since vim-textobj-parameter considers , followed by blankspace a parameter
     -- we need to check this manually for now
-    local arg = vim.fn.getreg('""')
-    if vim.fn.match(arg, '^,\\s*$') == 0 then
+    if is_cur_parameter_empty() then
         return
     end
     local cur_char = getcurchar()
@@ -55,7 +83,7 @@ local add_last_colon_if_missing = function(end_pos)
         return
     end
     normal('l')
-    local cur_char = getcurchar()
+    cur_char = getcurchar()
     if (cur_char == SEPERATOR) then
         return
     end
@@ -66,55 +94,174 @@ local set_indent = function(indent)
     normal('^d0i'..string.rep(' ', indent))
 end
 
-local is_end_bracket = function(char)
-    return vim.fn.match(settings.brackets.last, char) >= 0
+local only_whitespace_to_the_left = function()
+    local cur_pos = vim.fn.getcurpos()
+    normal('y0')
+    local left_text = vim.fn.getreg('""')
+    local is_match = vim.fn.match(left_text, '^\\s*$') >= 0
+    vim.fn.setpos('.', cur_pos)
+    return is_match
 end
 
-local seperate_motion_with_newlines = function(start_pos, orig_indent)
-    normal('li<CR><Esc>^') -- new line after motion
-    local indent
-    if is_end_bracket(getcurchar()) then
-        indent = orig_indent
-    else
-        indent = orig_indent+SHIFTWIDTH
+local only_whitespace_to_the_right = function()
+    local cur_pos = vim.fn.getcurpos()
+    normal('y$')
+    local left_text = vim.fn.getreg('""')
+    local is_match = vim.fn.match(left_text, '^.\\s*$') >= 0
+    vim.fn.setpos('.', cur_pos)
+    return is_match
+end
+
+local go_to_start_of_first_overlapping_parameter = function(region)
+    vim.fn.setpos('.', region.first)
+    select_and_return_outer_parameter()
+end
+
+local go_to_end_of_last_overlapping_parameter = function(region)
+    vim.fn.setpos('.', region.last)
+    select_and_return_outer_parameter()
+    normal('`]')
+end
+
+local add_newline = function()
+    normal('a<CR><Esc>')
+end
+
+local nohlsearch = function()
+    vim.cmd('nohlsearch')
+end
+
+local remove_all_indent = function()
+    vim.cmd('s/^\\s*//')
+    nohlsearch()
+end
+
+local are_pos_equal = function(pos1, pos2)
+    for i=1, 3 do
+        if pos1[i] ~= pos2[i] then
+            return false
+        end
     end
-    set_indent(indent)
-    vim.fn.setpos('.', start_pos)
-    normal('ya,`[', {noremap = true})
-    if vim.fn.getcurpos()[3] > 1 then
+    return true
+end
+
+local is_at_place_of_line = function(place)
+    local cur_pos = vim.fn.getcurpos()
+    normal(place) -- go to place of line
+    local place_pos = vim.fn.getcurpos()
+    local answer = are_pos_equal(cur_pos, place_pos)
+    vim.fn.setpos('.', cur_pos)
+    return answer
+end
+
+local is_at_start_of_line = function()
+    return is_at_place_of_line('0')
+end
+
+local is_at_end_of_line = function()
+    return is_at_place_of_line('$')
+end
+
+local get_next_char = function()
+    if is_at_end_of_line() then
+        return '\n'
+    end
+    normal('l')
+    local char = getcurchar()
+    normal('h')
+    return char
+end
+
+local remove_trailing_whitespace = function()
+    vim.cmd('s/\\s*$//')
+    nohlsearch()
+end
+
+local format_end_of_region = function(region, orig_indent)
+    go_to_end_of_last_overlapping_parameter(region)
+    if is_cur_parameter_empty() then
+    elseif get_next_char() == SEPERATOR then
+        normal('l')
+    elseif is_end_bracket(get_next_char()) then
+        select_and_return_inner_parameter()
+        normal('`]')-- go to end of inner argument
+        if settings.add_seperator_for_last_parameter then
+            normal('a'..SEPERATOR..'<Esc>')
+        end
+        if not settings.new_line_before_last_bracket then
+            return
+        end
+    else
+        error("unexpected scenario after region")
+    end
+    add_newline()
+    remove_trailing_whitespace()
+    remove_all_indent()
+    local new_indent
+    if is_end_bracket(getcurchar()) then
+        new_indent = orig_indent
+    else
+        new_indent = orig_indent+SHIFTWIDTH
+    end
+    set_indent(new_indent)
+end
+
+local format_start_of_region = function(region, orig_indent)
+    go_to_start_of_first_overlapping_parameter(region)
+    if not only_whitespace_to_the_left() then
         normal('i<CR><Esc>') -- new line
         set_indent(orig_indent+SHIFTWIDTH)
+
+        -- remove trailing whitespace on this and previous line
+        remove_trailing_whitespace()
+        local cur_pos = vim.fn.getcurpos()
+        normal('k')
+        remove_trailing_whitespace()
+        vim.fn.setpos('.', cur_pos)
     end
 end
 
-local is_last_arg = function()
-    local cur_line = vim.fn.getcurpos()[2]
-    return vim.fn.search([[,.*\S]], 'znc', cur_line) == 0
+local seperate_motion_with_newlines = function(region, orig_indent)
+    format_end_of_region(region, orig_indent)
+    format_start_of_region(region, orig_indent)
 end
 
-local seperate_args_with_newlines = function(orig_indent)
-    while {true} do
-        normal('ya,`]', {noremap = false}) -- go to , after first arg
-        if is_last_arg() then
+local is_last_parameter_on_line = function()
+    local cur_line = vim.fn.getcurpos()[2]
+    return vim.fn.search([[,.*\S]], 'znc', cur_line) == 0 --TODO correct?
+end
+
+local seperate_parameters_with_newlines = function(orig_indent)
+    while true do
+        local cur_pos = vim.fn.getcurpos()
+        select_and_return_inner_parameter()
+        normal('`]f'..SEPERATOR) -- go to , after first parameter
+        local new_pos = vim.fn.getcurpos()
+        if are_pos_equal(cur_pos, new_pos) then
+            error("couldn't not format current given region")
+        end
+        if is_last_parameter_on_line() then
+            -- TODO check to add last seperator
             break
         end
-        normal('hf i<CR><Esc>') -- new line for next arg
+        add_newline()
+        remove_trailing_whitespace()
         set_indent(orig_indent+SHIFTWIDTH)
     end
 end
 
-local is_arg = function()
-    normal('ya,', {noremap = false})
+local is_parameter = function()
+    select_and_return_outer_parameter()
     return vim.fn.getpos("'[")[3] ~= vim.fn.getpos("']")[3]
 end
 
-local are_ends_args = function(region)
+local are_ends_parameters = function(region)
     vim.fn.setpos('.', region.first)
-    if not is_arg() then
+    if not is_parameter() then
         return false
     end
     vim.fn.setpos('.', region.last)
-    if not is_arg() then
+    if not is_parameter() then
         return false
     end
     return true
@@ -173,6 +320,35 @@ local escape_pattern = function(pattern)
     return vim.fn.substitute(pattern, '[', '\\\\[', '')
 end
 
+local avoid_seperators_at_ends_of_region = function(region)
+    vim.fn.setpos('.', region.first)
+    while (getcurchar() == SEPERATOR) do
+        if is_at_end_of_line() then
+            vim.cmd('echoerr "got invalid region to format"')
+            return nil
+        end
+        normal('l')
+    end
+    region.first = vim.fn.getpos('.')
+
+    vim.fn.setpos('.', region.last)
+    while (getcurchar() == SEPERATOR) do
+        if is_at_start_of_line() then
+            vim.cmd('echoerr "got invalid region to format"')
+            return nil
+        end
+        normal('h')
+    end
+    region.last = vim.fn.getpos('.')
+
+    if region.first[3] > region.last[3] then
+        vim.cmd('echoerr "got invalid region to format"')
+        return nil
+    end
+
+    return region
+end
+
 local get_region = function(mode)
     local first, last
     if (mode == 'char') then
@@ -184,24 +360,51 @@ local get_region = function(mode)
     else
         return
     end
-    return {first = first, last = last}
+    local region = {first = first, last = last}
+    region = avoid_seperators_at_ends_of_region(region)
+    return region
+end
+
+local go_to_first_parameter_of_expression = function()
+    local cur_pos = vim.fn.getcurpos()
+    local new_pos
+    while true do
+        if getcurchar() == SEPERATOR then
+            normal('h')
+        end
+        select_and_return_outer_parameter()
+        new_pos = vim.fn.getcurpos()
+        if are_pos_equal(cur_pos, new_pos) then
+            return
+        end
+        cur_pos = new_pos
+    end
+end
+
+local go_to_start_bracket = function()
+    go_to_first_parameter_of_expression()
+    normal('b')
+end
+
+local get_orig_indent = function()
+    go_to_start_bracket()
+    return vim.fn.indent('.')
 end
 
 revj.format_region = function(mode)
-    local orig_indent = vim.fn.indent('.')
     local region = get_region(mode)
     if region == nil then
         return
     end
-    if not are_ends_args(region) then
-        vim.cmd("echoerr 'Motion ends do not overlap with args'")
+    local orig_indent = get_orig_indent()
+    print('region', region.first[3], region.last[3])
+    print('orig_indent', orig_indent)
+    if not are_ends_parameters(region) then
+        vim.cmd("echoerr 'Motion ends do not overlap with parameters'")
         return
     end
-    if settings.new_line_before_last_bracket then
-        add_last_colon_if_missing(region.last)
-    end
-    seperate_motion_with_newlines(region.first, orig_indent)
-    seperate_args_with_newlines(orig_indent)
+    seperate_motion_with_newlines(region, orig_indent)
+    seperate_parameters_with_newlines(orig_indent)
 end
 
 revj.format_line = function()
