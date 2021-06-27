17e21749b4edb5cad59dc5610bd6fadf78314602
Axel Dahlberg <git@valleymnt.com>
2021-06-27 13:26:35 +0200

    removed local shiftwidth test

--- tests/plenary/revj_spec.lua
+++ tests/plenary/revj_spec.lua
@@ -1,14 +1,9 @@
 local function assert_scenario(scenario)
-    -- print(vim.o.runtimepath)
-    -- vim.cmd('map')
-    -- print(vim.g.mapleader)
-    print(vim.fn.shiftwidth())
     vim.api.nvim_buf_set_lines(0, 0, -1, true, vim.fn.split(scenario.initial_buffer, '\n'))
     for _, command in ipairs(scenario.commands) do
         local keys = vim.api.nvim_replace_termcodes(command, true, false, true)
         vim.api.nvim_feedkeys(keys, 'xm', true)
     end
-    print(vim.fn.shiftwidth())
     local current_buffer = vim.fn.join(vim.api.nvim_buf_get_lines(0, 0, -1, true), '\n')
     assert.are.equal(current_buffer, scenario.expected_buffer)
 end
@@ -17,7 +12,6 @@ describe("revJ", function()
     it("normal", function()
         assert_scenario{
             initial_buffer = "some_func([1, 2], 1, 2, 3, True, lst=[], kw1=False, d={2, 3})",
-            -- commands = {":lua require('revj').format_line()<CR>"},
             commands = {",j"},
             expected_buffer = [[some_func(
     [1, 2],
@@ -29,24 +23,6 @@ describe("revJ", function()
     kw1=False,
     d={2, 3},
 )]],
-        }
-    end)
-
-    it("local shiftwidth", function()
-        assert_scenario{
-            initial_buffer = "some_func([1, 2], 1, 2, 3, True, lst=[], kw1=False, d={2, 3})",
-            -- commands = {":set shiftwidth=2", ":lua require('revj').format_line()<CR>"},
-            commands = {":set shiftwidth=2", ",j"},
-            expected_buffer = [[some_func(
-  [1, 2],
-  1,
-  2,
-  3,
-  True,
-  lst=[],
-  kw1=False,
-  d={2, 3},
-}]],
         }
     end)
 end)
