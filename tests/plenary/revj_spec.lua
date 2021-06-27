7a80955a353dee5108d236216f96e3db77e43677
Axel Dahlberg <git@valleymnt.com>
2021-06-27 13:31:46 +0200

    added test for local shiftwidth

--- tests/plenary/revj_spec.lua
+++ tests/plenary/revj_spec.lua
@@ -1,9 +1,11 @@
 local function assert_scenario(scenario)
+    print('shiftwidth before', vim.fn.shiftwidth())
     vim.api.nvim_buf_set_lines(0, 0, -1, true, vim.fn.split(scenario.initial_buffer, '\n'))
     for _, command in ipairs(scenario.commands) do
         local keys = vim.api.nvim_replace_termcodes(command, true, false, true)
         vim.api.nvim_feedkeys(keys, 'xm', true)
     end
+    print('shiftwidth after', vim.fn.shiftwidth())
     local current_buffer = vim.fn.join(vim.api.nvim_buf_get_lines(0, 0, -1, true), '\n')
     assert.are.equal(current_buffer, scenario.expected_buffer)
 end
@@ -23,6 +25,23 @@ describe("revJ", function()
     kw1=False,
     d={2, 3},
 )]],
+        }
+    end)
+
+    it("local shiftwidth", function()
+        assert_scenario{
+            initial_buffer = "some_func([1, 2], 1, 2, 3, True, lst=[], kw1=False, d={2, 3})",
+            commands = {":set shiftwidth=2", ",j"},
+            expected_buffer = [[some_func(
+  [1, 2],
+  1,
+  2,
+  3,
+  True,
+  lst=[],
+  kw1=False,
+  d={2, 3},
+}]],
         }
     end)
 end)
